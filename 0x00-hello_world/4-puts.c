67ce3f56eb06765026e265ed56e38955bc647c68
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-03-12 20:07:46 +0300

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
-#include stdio.h
-/*
- * programme to print hello world
- *
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
  */
- int main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"
-			return(0)#include stdio.h
-/*
- * programme to print hello world
- *
- */
- int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle"
-                        return(0);
-}
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
