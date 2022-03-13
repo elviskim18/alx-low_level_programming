a6ac2de382acb72b8dee29475ceaabe533e1d066
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-03-13 14:35:12 +0300

    corect

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return:0
@@ -7,15 +6,18 @@
 int main(void)
 {
 	int i, y;
+
 	for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
 	}
+
 	for (y = 'a'; y <= 'f'; y++)
 	{
 		putchar (y);
 	}
 
-putchar('\n');
-return (0);
+   putchar('\n');
+
+   return (0);
 }
