c340f215119a22ccde5b7d70429a3b3339ea1ebf
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-03-24 23:33:06 +0300

    correction

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,19 @@
 #include "main.h"
-
 /**
- * reverse_array - a function that takes a pointer to an int as parameter and
- * @a: the tab integer
- * @n: the number
- *
- * Return: 1 or 0
- */
+ * reverse_array - reverse a int array
+ * @a: pointer
+ * @n: array length
+ * Return: void
+*/
 
 void reverse_array(int *a, int n)
 {
-	int i = 0, taille, k;
-	int c;
+int i = 0, half;
 
-	taille = n - 1;
-	k = taille / 2;
-
-	while (i <= k)
-	{
-		c = a[i];
-		a[i] = a[taille];
-		a[taille] = c;
-		i++;
-		taille--;
-	}
+for (half = n / 2; half > 0; half--, i++)
+{
+	a[n - i - 1] += a[i];
+	a[i] = a[n - i - 1] - a[i];
+	a[n - i - 1] = a[n - i - 1] - a[i];
+}
 }
