41c3152858c6ffc1d2c7509d70b9ae3be7e22ccb
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-03-29 19:51:34 +0300

    correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,30 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
 /**
- *
- * 0-memset
- * _memset -a function
- * @s: the char
- * @b the char
+ * _memset - a function ...
+ * @s: the chaine
+ * @b: the char
  * @n: the int
  *
  * Return: 1 or 0
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	
-	for(i = 0, i < n; i++)
+
+	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
 }
+
