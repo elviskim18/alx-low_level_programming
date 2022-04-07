995b64e3cc35cfaeaa1750f8ef3558648ff7f63d
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-04-07 20:02:38 +0300

    correct

--- 0X0B-malloc_free/0-create_array.c
+++ 0X0B-malloc_free/0-create_array.c
@@ -1,21 +1,39 @@
-#include <stdlib.h>
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdlib.h>
 
 /**
- * *create_array - creates an array of chars,
- * and initializes it with a specific char.
- * @size: size of the array
- * @c: char to initialize
- * Return: pointer to the array initialized or NULL
+ * create_array - a function ...
+ * @size: the int
+ * @c: the char
+ *
+ * Return: 1 or 0
  */
 
 char *create_array(unsigned int size, char c)
 {
-char *m = malloc(size);
-if (size == 0 || m == 0)
-	return (0);
+	char *tab;
+	unsigned int i;
 
-while (size--)
-	m[size] = c;
+	if (size == 0)
+		return (NULL);
+	tab = malloc(size * sizeof(char));
+	if (tab != NULL)
+	{
+		for (i = 0; i < size; i++)
+			tab[i] = c;
+	}
 
-return (m);
+	return (tab);
 }
+
