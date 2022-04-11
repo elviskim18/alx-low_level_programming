3bd8fdaf8367e343f2ccae458b8a7f589653274b
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-04-11 23:12:05 +0300

    correct

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,33 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - print the name of the file that was compiled
+ * Return: int
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	int i = 0;
-
-	while (__FILE__[i])
-	{
-		_putchar(__FILE__[i]);
-		i++;
-	}
-	_putchar('\n');
-	/*printf("%s\n",__FILE__);*/
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
