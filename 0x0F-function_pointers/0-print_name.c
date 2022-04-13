921a4338f0373df7e9ff151d08884805111a7c28
Elvis Kimani <Betty Kimbo@DESKTOP-A3D1RAS>
2022-04-13 23:02:23 +0300

    correct

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,28 @@
-#include "function_pointers.h"
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
 
+#include<stddef.h>
 /**
- * print_name - prints a name
- * @name: string
- * @f: function
+ * print_name - a function ...
+ * @name: the chaine
+ * @f: the function
+ *
+ * Return: 1 or 0
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
-	f(name);
+	if (f == NULL)
+		return;
+	(*f)(name);
 }
+
